423d88a3b7d1d8c31deb69732cbd4fb9ad20bb4f
Jason Feng <jason@identified.com>
2012-04-17 14:03:10 -0700

    Do a little metaprogramming magic for nicer syntax.

--- sensei-rb.rb
+++ sensei-rb.rb
@@ -1,4 +1,6 @@
 module Sensei
+  CONSTRUCT_BLOCK_KEY='in_sensei_construct'
+  
   module Operators
     def &(x)
       BoolQuery.new(:operands => [self.to_sensei, x.to_sensei], :operation => :must)
@@ -13,8 +15,32 @@ module Sensei
     end
 
     def boost! amt
-      @options[:boost] = amt
-      self
+      self.to_sensei.tap do |x| x.options[:boost] = amt end
+    end
+  end
+
+  [Hash, Range].each do |klass|
+    conflicts = klass.instance_methods & Operators.instance_methods
+    non_conflicts = Operators.instance_methods - conflicts
+
+    conflicts.each do |override_method|
+      klass.class_eval do
+        define_method(:"#{override_method}_with_sensei_construct") do |*args|
+          if Thread.current[Sensei::CONSTRUCT_BLOCK_KEY]
+            self.to_sensei.send(override_method, *args)
+          else
+            self.send(:"#{override_method}_without_sensei_construct", *args)
+          end
+        end
+        
+        alias_method_chain override_method, :sensei_construct
+      end
+    end
+
+    non_conflicts.each do |meth|
+      klass.class_eval do
+        define_method(meth.to_sym) { |*args| self.to_sensei.send(meth, *args) }
+      end
     end
   end
 
@@ -33,6 +59,15 @@ module Sensei
 
     def to_sensei
       self
+    end
+
+    def self.construct &block
+      Thread.current[Sensei::CONSTRUCT_BLOCK_KEY] = true
+      begin
+        block.call.to_sensei
+      ensure
+        Thread.current[Sensei::CONSTRUCT_BLOCK_KEY] = false
+      end
     end
   end
 
@@ -67,7 +102,7 @@ end
 class Hash
   def to_sensei
     field, value = self.first
-    if value.is_a? String
+    if [String, Fixnum, Float, Bignum].member?(value.class)
       Sensei::TermQuery.new(:field => field, :value => value)
     else
       value.to_sensei(field)
