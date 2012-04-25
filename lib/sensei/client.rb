4314ced85ff2cda5b7e20195d559f2bb93319088
Jason Feng <jason@identified.com>
2012-04-24 18:20:47 -0700

    Simplify selections, add a method for making facet requests for when we have selected elements on multiple fields.

--- lib/sensei/client.rb
+++ lib/sensei/client.rb
@@ -3,12 +3,12 @@ module Sensei
     def initialize optargs={}
       @query = optargs[:query]
       @facets = (optargs[:facets] || {})
-      @selections = (optargs[:selections] || [])
-      @other_options = optargs.dup.keep_if {|k,v| ![:query, :facets].member?(k)}
+      @selections = (optargs[:selections] || {})
+      @other_options = optargs.dup.keep_if {|k,v| ![:query, :facets, :selections].member?(k)}
     end
 
     DEFAULT_FACET_OPTIONS = {:max => 6, :minCount => 1}
-    
+
     # Add a desired facet to the results
     def facet(field, options={})
       @facets[field] = DEFAULT_FACET_OPTIONS.merge(options)
@@ -19,14 +19,10 @@ module Sensei
       @query=q.to_sensei.to_h
       self
     end
-    
+
     # Do facet selection
-    def selection(field, values=[])
-      if field.is_a? Hash
-        @selections << field
-      else
-        @selections << {:terms => {field => {:values => values, :operator => "or"}}}
-      end
+    def selection(fields = {})
+      @selections.merge!(fields)
       self
     end
 
@@ -34,12 +30,13 @@ module Sensei
       @other_options.merge!(opts)
       self
     end
-    
+
     def to_h
       out = {}
       (out[:query] = @query) if @query
       (out[:facets] = @facets) if @facets.count > 0
-      (out[:selections] = @selections) if @selections.count > 0
+      selections = @selections.map { |field, terms| {:terms => {field => {values: terms, :operator => "or"}}} }
+      (out[:selections] = selections) if selections.count > 0
       out.merge!(@other_options)
       out
     end
@@ -55,6 +52,34 @@ module Sensei
       req = Curl::Easy.new(Webster::Config.sensei_url)
       req.http_post(self.to_h.to_json)
       JSON.parse(req.body_str)
+    end
+
+    # This method performs several separate queries with different
+    # selection settings as necessary.  This is needed to perform
+    # the common interaction pattern for faceted search, in which
+    # it is desired that selections from other facets affect a
+    # particular facet's counts, but a facet's own selections do
+    # facet do not affect its own counts.
+    def select_search
+      all_selection_results = search
+      facet_requests.map(&:search).each do |result|
+        field, counts = result['facets'].first
+        all_selection_results['facets'][field] += counts
+      end
+
+      Hash[*all_selection_results['facets'].map do |k,v|
+             [k, v.uniq_by{|x| x['value']}]
+           end.flatten(1)]
+    end
+
+    # This method builds the requests necessary to perform the `select_search' method.
+    def facet_requests
+      @selections.map do |field, values|
+        Sensei::Client.new(:query => @query,
+                           :facets => @facets.dup.keep_if {|name, opts| name==field},
+                           :selections => @selections.dup.keep_if {|name, opts| name != field},
+                           :size => 0)
+      end
     end
   end
 end
